fe7f2285be9387fb9b2dfe0a875b2d6f8fcc8f66
Caleb Fangmeier <cfangmeier74@gmail.com>
2014-02-17 19:44:55 -0600

    update to xmlparser to be more complete(still not finished) Update to format of default_data.xml

--- src/scorpion/localdb/db.py
+++ src/scorpion/localdb/db.py
@@ -7,24 +7,27 @@ Created on Feb 7, 2014
 import os
 import sqlalchemy as sql
 import sqlalchemy.orm as orm
+
 import scorpion.config as config
 import dbobjects
-
+import xmlparser
 
 
 def put_some_data(session):
-
-    return krogada
+    objects = xmlparser.get_objects()
+    session.add_all(objects)
 
 def reset_db():
     if os.path.exists(config.local_db):
         os.remove(config.local_db)
+        print 'removed old db'
     
-    engine = sql.create_engine('sqlite:///scorpion.db', echo = True)
+    engine = sql.create_engine('sqlite:///'+config.local_db, echo = True)
     dbobjects.create_tables(engine)
     Session = orm.sessionmaker(bind=engine)
     session = Session()
     put_some_data(session)
+    session.commit()
     
 if __name__ == "__main__":
     reset_db()
--- src/scorpion/localdb/xmlparser.py
+++ src/scorpion/localdb/xmlparser.py
@@ -22,19 +22,58 @@ def build_brand(attrs):
     b = dbo.Brand()
     b.name = attrs['name']
     b.country = attrs['country']
-    b.yearfounded = attrs['yearfounded']
+    b.yearfounded = int(attrs['yearfounded'])
     objects.append(b)
 
 def build_liquor(attrs):
     global objects
-    b = dbo.Liquor()
-    b.name = attrs['name']
-    b.density = attrs['density']
-    b.abv = attrs['abv']
-    b.bottleweight = attrs['bottleweight']
-    b.upc = attrs['upc']
-    b.type = [t for t in objects 
-              if type(t) == dbo.Type and t.name == attrs['abv']][0]
+    l = dbo.Liquor()
+    l.name = attrs['name']
+    l.density = float(attrs['density'])
+    l.abv = float(attrs['abv'])
+    l.type = [t for t in objects 
+              if type(t) == dbo.Type and t.name == attrs['type']][0]
+    l.brand = [b for b in objects
+               if type(b) == dbo.Brand and b.name == attrs['brand']][0]
+    objects.append(l)
+
+def build_liquorsku(attrs):
+    global objects
+    l = dbo.LiquorSKU()
+    l.bottleweight = float(attrs["bottleweight"])
+    l.volume = float(attrs['volume'])
+    l.upc = attrs['upc']
+    (brand,liquor) = attrs['liquor'].split('|')
+    l.liquor = [l for l in objects 
+                if type(l) == dbo.Liquor and l.name == liquor and l.brand.name == brand][0]
+    objects.append(l)
+
+def build_drink(attrs):
+    global objects
+    d = dbo.Drink()
+    d.name = attrs['name']
+    d.description = attrs['description']
+    d.mixinstructions = attrs['mixinstructions']
+    d.glasstype = attrs['glasstype']
+    objects.append(d)
+    
+def build_liquoringredient(attrs):
+    global objects
+    li = dbo.LiquorIngredient()
+    li.measure = float(attrs['measure'])
+    li.drink = [d for d in objects if type(d) == dbo.Drink and d.name == attrs['drink']][0]
+    (brand,liquor) = attrs['liquor'].split('|')
+    li.liquor = [l for l in objects 
+                 if type(l) == dbo.Liquor and l.name == liquor and l.brand.name == brand][0]
+    objects.append(li)
+    
+def build_genliquoringredient(attrs):
+    global objects
+    gli = dbo.GenLiquorIngredient()
+    gli.measure = float(attrs['measure'])
+    gli.drink = [d for d in objects if type(d) == dbo.Drink and d.name == attrs['drink']][0]
+    gli.type = [t for t in objects if type(t) == dbo.Type and t.name == attrs['type']][0]
+    objects.append(gli)
 
 def start_element(name, attrs):
     
@@ -48,17 +87,23 @@ def start_element(name, attrs):
         build_drink(attrs)
     elif name == "LiquorIngredient":
         build_liquoringredient(attrs)
+    elif name == "GenLiquorIngredient":
+        build_genliquoringredient(attrs)
 
 
-def get_objects():
-    if not os.path.exists(xml_path): return False
-    f = open(xml_path,'r')
+def get_objects(path = xml_path):
+    global objects
+    if not os.path.exists(path): return False
+    f = open(path,'r')
     data = f.read()
     f.close()
     parser = expat.ParserCreate()
     parser.StartElementHandler = start_element
     parser.Parse(data)
     
+    ret = objects
+    objects = []
+    return ret
     
 if __name__ == "__main__":
-    get_objects()
+    print get_objects()
