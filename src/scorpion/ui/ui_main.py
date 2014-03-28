526e16e94d222fdbc6632cd3ab510bc7ff2115f3
Caleb Fangmeier <cfangmeier74@gmail.com>
2014-03-28 12:08:12 -0500

    Add some more more kivy code to match the ui mockups. Very much a work in progress.

--- src/scorpion/ui/ui_main.py
+++ src/scorpion/ui/ui_main.py
@@ -6,7 +6,7 @@ Created on Mar 24, 2014
 
 from kivy.app import App
 from kivy.uix.screenmanager import Screen
-from kivy.properties import ObjectProperty  # @UnresolvedImport
+from kivy.properties import ObjectProperty
 from kivy.uix.button import Button
 
 
@@ -19,10 +19,20 @@ class StartScreen(Screen):
     icon_container = ObjectProperty(None)
     def __init__(self, **kwargs):
         super(StartScreen, self).__init__(**kwargs)
+        self.n = 1
     
-    def on_enter(self, *args):
-        Screen.on_enter(self, *args)
-        self.icon_container.add_widget(ScrollButton(text = 'hello'))
+    def on_pre_enter(self, *args):
+        Screen.on_pre_enter(self, *args)
+        self.icon_container.clear_widgets()
+        for _ in range(10):
+            self.icon_container.add_widget(ScrollButton(text = 'hello'+str(self.n)))
+            self.n += 1
+
+class LiquorScreen(Screen):
+    pass
+
+class MixingScreen(Screen):
+    pass
 
 class MainApp(App):
     
